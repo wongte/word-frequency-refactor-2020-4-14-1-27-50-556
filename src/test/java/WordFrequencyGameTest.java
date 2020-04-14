35b045c7b3714c26bad737517381aeb57b4cd3e8
Terrence Wong <wongte@oocl.com>
2020-04-14 15:43:14 +0800

    [Terrence] - remove validate method in test cases

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -1,15 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class WordFrequencyGameTest {
 
+    private WordFrequencyGame game;
+
+    @Before
+    public void setUp() {
+        game = new WordFrequencyGame();
+    }
+
     @Test
     public void should_get_the_1_when_input_the() {
         //Given
         String inputSentence = "the";
         String expectResult = "the 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
+        //When
+        String result = game.getResult(inputSentence);
+        //Then
+        assertEquals(result, expectResult);
     }
 
     @Test
@@ -17,7 +28,10 @@ public class WordFrequencyGameTest {
         //Given
         String inputSentence = "the is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
+        //When
+        String result = game.getResult(inputSentence);
+        //Then
+        assertEquals(result, expectResult);
     }
 
     @Test
@@ -25,7 +39,10 @@ public class WordFrequencyGameTest {
         //Given
         String inputSentence = "the      is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
+        //When
+        String result = game.getResult(inputSentence);
+        //Then
+        assertEquals(result, expectResult);
     }
 
     @Test
@@ -33,15 +50,21 @@ public class WordFrequencyGameTest {
         //Given
         String inputSentence = "the   \n   is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
+        //When
+        String result = game.getResult(inputSentence);
+        //Then
+        assertEquals(result, expectResult);
     }
 
     @Test
-    public void should_pracess_two_same_words_with_sorted() {
+    public void should_process_two_same_words_with_sorted() {
         //Given
         String inputSentence = "the the is";
         String expectResult = "the 2\nis 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
+        //When
+        String result = game.getResult(inputSentence);
+        //Then
+        assertEquals(result, expectResult);
     }
 
     @Test
@@ -49,11 +72,6 @@ public class WordFrequencyGameTest {
         //Given
         String inputSentence = "the is is";
         String expectResult = "is 2\nthe 1";
-        validate_Input_words_process_to_expected_word(inputSentence, expectResult);
-    }
-
-    private void validate_Input_words_process_to_expected_word(String inputSentence, String expectResult) {
-        WordFrequencyGame game = new WordFrequencyGame();
         //When
         String result = game.getResult(inputSentence);
         //Then
